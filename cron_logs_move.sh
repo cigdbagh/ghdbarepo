b3d0ec02897c917919f727553466e5be40f492ed
Alex Lamont <alamont@ciginsurance.com>
2017-01-26 20:01:06 -0800

    delete Please enter the commit message for your changes. Lines starting

--- cron_logs_move.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-# Name     :  cron_logs_move.sh
-# Author   :  Ramesh Kalyandrug
-# created  :  11/07/16
-
-CURR_DATE=`date +%b%Y%d`
-echo $CURR_DATE
-mkdir -p /tmp/logs/old/${CURR_DATE}
-mv /tmp/logs/*.log /tmp/logs/old/${CURR_DATE}/
